99d482758fc326cc8405c508988c62d667190864
LECODEBAR <sc.rouault.pro@gmail.com>
2025-08-07 09:59:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-document.getElementById("enterBtn").addEventListener("click", () => {
-  showPopup("maintenance");
-});
-
-function showPopup(type) {
-  document.getElementById(`popup${capitalize(type)}`).classList.remove("hidden");
+function openPopup(type) {
+  document.querySelector('.overlay').style.display = 'block';
+  document.getElementById('popup-' + type).style.display = 'flex';
 }
 
-function closePopup(type) {
-  document.getElementById(`popup${capitalize(type)}`).classList.add("hidden");
-}
-
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+function closePopup() {
+  document.querySelector('.overlay').style.display = 'none';
+  document.querySelectorAll('.popup').forEach(p => p.style.display = 'none');
 }
