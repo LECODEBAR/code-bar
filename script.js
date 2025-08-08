109fa125da185f8dfc347c72efcc46200d2cee13
LECODEBAR <sc.rouault.pro@gmail.com>
2025-08-08 10:40:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function showPopup(type) {
-  const popup = document.getElementById(`popup-${type}`);
-  if (popup) {
-    popup.style.display = 'flex';
-  }
-}
 
-function closePopup(type) {
-  const popup = document.getElementById(`popup-${type}`);
-  if (popup) {
-    popup.style.display = 'none';
-  }
-}
